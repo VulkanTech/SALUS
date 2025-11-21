5bc8658569b1c8f0a21dfdf4888ffa7d404ede0b
Felipe <felipe.feiitosa08@gmail.com>
2025-11-21 03:00:23 -0300

    Atualização ConsultaService para usar ConsultaRepository

--- src/main/java/com/vulkantech/salus/service/ConsultaService.java
+++ src/main/java/com/vulkantech/salus/service/ConsultaService.java
@@ -2,50 +2,46 @@ package com.vulkantech.salus.service;
 import com.vulkantech.salus.model.Consulta;
 import com.vulkantech.salus.model.Medico;
 import com.vulkantech.salus.model.Paciente;
+import com.vulkantech.salus.repository.ConsultaRepository;
 import org.springframework.stereotype.Service;
 import com.vulkantech.salus.exception.ConsultaNaoEncontradaException;
 import com.vulkantech.salus.exception.ConflitodeHorarioException;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.ArrayList;
 
 @Service
 public class ConsultaService {
-    List<Consulta> consultas = new ArrayList<>();
+    private final ConsultaRepository consultaRepository;
+
+    public ConsultaService(ConsultaRepository consultaRepository) {
+        this.consultaRepository = consultaRepository;
+    }
 
     // CREATE
     public Consulta addConsulta(Medico medico, Paciente paciente, LocalDateTime dataHora ) {
-        for (Consulta c : consultas) {
-            if (c.getMedico().equals(medico) && c.getDataHora().equals(dataHora) && !c.isCancelar()) {
-                throw new RuntimeException("Esse médico já tem consulta nesse horário.");
-            }
-            else if (c.getPaciente().equals(paciente) && c.getDataHora().equals(dataHora) && !c.isCancelar()) {
-                throw new RuntimeException("Esse paciente já tem uma consulta marcada nesse horário.");
-            }
-            else if (c.getMedico().equals(medico) && c.getPaciente().equals(paciente) && c.getDataHora().equals(dataHora) && !c.isCancelar()) {
-                throw new RuntimeException("Essa consulta já está marcada nesse horário.");
-            }
+        if (consultaRepository.existsByMedico_CrmAndDataHora(medico.getCrm(), dataHora)) {
+            throw new ConflitodeHorarioException("Esse médico já tem consulta neste horário.");
+        }
+
+        else if (consultaRepository.existsByPaciente_CpfAndDataHora(paciente.getCpf(), dataHora)) {
+            throw new ConflitodeHorarioException("Esse paciente já tem consulta neste horário.");
         }
 
         Consulta novaConsulta = new Consulta(medico, paciente, dataHora);
-        consultas.add(novaConsulta);
-        return novaConsulta;
+        return consultaRepository.save(novaConsulta);
     }
 
     // READ
     public List<Consulta> getConsultas() {
-        return consultas;
+        return consultaRepository.findAll();
     }
 
     public List<Consulta> listarConsultasDoMedico(Medico medico) {
-        List<Consulta> lista = new ArrayList<>();
-        for (Consulta c : consultas) {
-            if (c.getMedico().equals(medico)) {
-                lista.add(c);
-            }
-        }
-        return lista;
+        return consultaRepository.findAll()
+                .stream()
+                .filter(c -> c.getMedico().equals(medico))
+                .toList();
     }
 
     // UPDATE
@@ -54,5 +50,6 @@ public class ConsultaService {
     // DELETE
     public void cancelarConsulta(Consulta consulta) {
         consulta.cancelar();
+        consultaRepository.save(consulta);
     }
 }
